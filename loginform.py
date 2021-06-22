f793e034673de661825c5210a43d7751cf7b84b4
WeiQi-01 <weiqiwwq99@gmail.com>
2021-06-22 21:06:01 +0800

    loginform py file renamed Nric to NRIC and address to Address for wtforms

--- loginform.py
+++ loginform.py
@@ -7,7 +7,7 @@ class CreateLoginUserForm(Form):
     Username = StringField('Username:', [validators.Length(min=1, max=150), validators.DataRequired()])
     Password = PasswordField('Account Password:', [validators.DataRequired()])
     Phone_Number = StringField('Phone Number:', [validators.DataRequired(),validators.Regexp(regex='\d{4}[-.\s]?\d{4}$',message='phone number can only have 8 digit !'),validators.Regexp(regex='[0-9]',message='Only numeric number')])
-    NRIC=StringField('Nric: ',[validators.DataRequired()])
+    NRIC=StringField('NRIC: ',[validators.DataRequired()])
     Email=StringField('Email Address: ',[validators.DataRequired()])
     Security_Questions = SelectField('Security Questions:', [validators.DataRequired()],
                            choices=[('', 'Question 1 (Select One)'), ('Where were you when you had your first kiss? ', 'Where were you when you had your first kiss?'),
@@ -18,7 +18,7 @@ class CreateLoginUserForm(Form):
                                     ('What is your oldest sibling’s birthday month and year? (e.g., January 1999)', 'What is your oldest sibling’s birthday month and year? (e.g., January 1999)'),
                                     ('What is the middle name of your youngest child?', 'What is the middle name of your youngest child?')], default='')
     Answers=StringField('Question 1 Answer: ',[validators.DataRequired()])
-    Address=TextAreaField('address:',[validators.DataRequired()])
+    Address=TextAreaField('Address:',[validators.DataRequired()])
     role=StringField('Role',default='Guest',render_kw = {'readonly':True})
 
 
